7361233b23f367cb1b22faf5093ac28d555e6200
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-09 23:36:42 -0500

    Creacion de la clase Quizz_Brain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank =  [Question(i["text"],i["answer"]) for i in question_data]
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+    question_number = 0
+    def __init__(self,question_list):
+        self.question_list = question_list
+        
+        
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q.{self.question_number+1}: {current_question.text} (True/False)?: ")
+        self.question_number+=1
